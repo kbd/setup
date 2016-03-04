402b7f4cbc8a989b1fdd20e059ceeddc5376388a
Keith Devens <keith@keithdevens.com>
2016-03-04 07:34:48 -0700

    Fix code style

--- tests/utils.py
+++ tests/utils.py
@@ -3,6 +3,6 @@ import sys
 
 
 def add_bin_to_path():
-    BINPATH = os.path.abspath(
+    binpath = os.path.abspath(
         os.path.join(os.path.dirname(__file__), '../HOME/bin'))
-    sys.path.append(BINPATH)
+    sys.path.append(binpath)
