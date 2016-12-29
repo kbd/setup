07e7626f120a603e82db5d73c7e6a4b543b74d60
Keith Devens <keith@keithdevens.com>
2016-12-28 20:47:20 -0700

    Remove z.sh symlink creation
    I neglected to remove this when switching from z to fasd

--- settings.py
+++ settings.py
@@ -133,9 +133,6 @@
             'xquartz',
         ],
         'post_install': [
-            # symlink 'z' into my local shell sources so it'll be 1. run automatically in my
-            # .bash_profile, 2. copied to servers by my copy_config_to_host script
-            'ln -sf `brew --prefix`/etc/profile.d/z.sh HOME/bin/shell_sources/',
             'update_shell.sh `brew --prefix`/bin/bash',  # set shell to homebrew'd bash
         ],
     },
