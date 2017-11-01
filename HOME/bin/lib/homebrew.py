eac29ce4e0387b30ec0105fffde0e002977af40c
Keith Devens <keith@keithdevens.com>
2017-11-01 03:03:35 -0600

    Delete the brew cache while cleaning up formulas

--- HOME/bin/lib/homebrew.py
+++ HOME/bin/lib/homebrew.py
@@ -69,7 +69,13 @@ def install_tap(tap):
 
 def cleanup_formulas():
     log.info("Running cleanup: formulas")
+    # from 'brew cleanup --help':
+    # If -s is passed, scrub the cache, removing downloads for even the latest
+    # versions of formulae. Note downloads for any installed formulae will still not be
+    # deleted. If you want to delete those too: rm -rf $(brew --cache)
     _execute(['brew', 'cleanup', '-s'])
+    log.info("Deleting brew cache")
+    _execute('rm -rf "$(brew --cache)"')
 
 
 def cleanup_casks():
