c79c5f169ed820d789a9aa9fedbd10b0b35fe75b
Keith Devens <keith@keithdevens.com>
2018-02-13 12:42:06 -0700

    Use single quotes for aliases when double quotes unnecessary

--- HOME/bin/shell_sources/aliases.sh
+++ HOME/bin/shell_sources/aliases.sh
@@ -13,44 +13,44 @@ export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM='auto'
 # ls colors I expect: exe=red, dir=blue, symlink=pink, pipe=yellow
-export LS_COLORS="ex=31:di=34:ln=35:pi=33"
+export LS_COLORS='ex=31:di=34:ln=35:pi=33'
 
 # ALIASES
-alias   -- -="cd -"
-alias     ..="cd .."
-alias    ...="cd ../.."
-alias   ....="cd ../../.."
-alias  .....="cd ../../../.."
-alias ......="cd ../../../../.."
+alias   -- -='cd -'
+alias     ..='cd ..'
+alias    ...='cd ../..'
+alias   ....='cd ../../..'
+alias  .....='cd ../../../..'
+alias ......='cd ../../../../..'
 
-alias ls="ls -h"
+alias ls='ls -h'
 alias l=ls
-alias la="ls -a"
-alias ll="ls -l"
-alias lla="ls -la"
+alias la='ls -a'
+alias ll='ls -l'
+alias lla='ls -la'
 
 alias wcl='wc -l'
 
 alias edit=\$EDITOR "$@"
 alias e=edit
-alias e.="e ."
+alias e.='e .'
 
 alias o=open
-alias o.="o ."
+alias o.='o .'
 
-alias grep="grep --color=auto"
+alias grep='grep --color=auto'
 alias g=grep
 
 alias h=history
 
-alias du="du -h"
+alias du='du -h'
 
 alias v=vim
 alias vi=vim
 
 alias py=ipython
 
-alias tcl="rlwrap tclsh"
+alias tcl='rlwrap tclsh'
 
 # PLATFORM SPECIFIC
 if [[ $PLATFORM == 'Darwin' ]]; then
@@ -63,16 +63,16 @@ if [[ $PLATFORM == 'Darwin' ]]; then
     alias tar=gtar
 
     # bsd ls
-    alias ls="ls -FG"
+    alias ls='ls -FG'
     # escape ls to ignore -F so you don't get directories with // at the end
-    alias lsd="\\ls -dG */"
-    alias lld="\\ls -hldG */"
+    alias lsd='\ls -dG */'
+    alias lld='\ls -hldG */'
 else
     # gnu ls
-    alias ls="ls -F --color"
+    alias ls='ls -F --color'
     # '--' necessary to correctly handle filenames beginning with -
     # bsd ls handles this correctly by default and doesn't allow --
     # indicator-style=none so you don't get directories with // at the end
-    alias lsd="ls -d --indicator-style=none -- */"
-    alias lld="ll -d --indicator-style=none -- */"
+    alias lsd='ls -d --indicator-style=none -- */'
+    alias lld='ll -d --indicator-style=none -- */'
 fi
