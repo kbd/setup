b9868d970feaf38ca3b250390ec8f8cf9e3ed9cc
Keith Devens <keith@keithdevens.com>
2018-06-08 12:02:24 -0600

    Update osquery ports sql

--- HOME/bin/osquery/ports.sql
+++ HOME/bin/osquery/ports.sql
@@ -1,8 +1,10 @@
 SELECT DISTINCT
 	process.name
+	, listening.address
 	, listening.port
 	, process.pid
 FROM processes AS process
 JOIN listening_ports AS listening
 	ON process.pid = listening.pid
-WHERE listening.address = '0.0.0.0';
+WHERE listening.address IS NOT null
+	AND listening.address <> '';
