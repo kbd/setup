ed4a90cf4ba8bbfb1395488a67820ace397d3290
Keith Devens <keith@keithdevens.com>
2018-12-14 18:31:54 -0700

    Put back GIT_EDITOR

--- HOME/bin/shell_sources/aliases.sh
+++ HOME/bin/shell_sources/aliases.sh
@@ -9,6 +9,7 @@ export PLATFORM=$(uname)
 export PAGER=less
 export LESS='-iM'  # smart-case searches and status bar
 export EDITOR=vim
+export GIT_EDITOR=vim
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export PYTHONDONTWRITEBYTECODE=1
