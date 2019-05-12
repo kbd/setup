037a4dd1360c0d26e8fe39abe804fe4ee1ba4cf2
Keith Devens <keith@keithdevens.com>
2019-05-12 15:21:10 -0600

    Make calls to defaults write consistent

--- conf/mac.sh
+++ conf/mac.sh
@@ -2,24 +2,24 @@
 
 ### trackpad settings ###
 for key in com.apple.AppleMultitouchTrackpad com.apple.driver.AppleBluetoothMultitouch.trackpad; do
-    defaults write $key Clicking -boolean True # touch to click
+    defaults write $key Clicking -bool true # touch to click
 
     # enable *both* methods of right clicking
-    defaults write $key TrackpadRightClick -boolean True # two finger tap
-    defaults write $key TrackpadCornerSecondaryClick -integer 2 # pushing to click in right corner
+    defaults write $key TrackpadRightClick -bool true # two finger tap
+    defaults write $key TrackpadCornerSecondaryClick -int 2 # pushing to click in right corner
 
     # disable "smart zoom" because it puts a delay on two-finger-tap right click
-    defaults write $key TrackpadTwoFingerDoubleTapGesture -boolean False
+    defaults write $key TrackpadTwoFingerDoubleTapGesture -bool false
 
-    defaults write $key TrackpadThreeFingerDrag -boolean True
+    defaults write $key TrackpadThreeFingerDrag -bool true
 done
 
 
 # disable dashboard
-defaults write com.apple.dashboard mcx-disabled -boolean True
+defaults write com.apple.dashboard mcx-disabled -bool true
 
 # http://www.defaults-write.com/enable-highlight-hover-effect-for-grid-view-stacks/
-defaults write com.apple.dock mouse-over-hilite-stack -boolean True
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
 
 # hot corners
@@ -36,44 +36,44 @@ defaults write com.apple.dock mouse-over-hilite-stack -boolean True
 # 12: Notification Center
 
 # bottom left: sleep
-defaults write com.apple.dock wvous-bl-corner -integer 10
-defaults write com.apple.dock wvous-bl-modifier -integer 0
+defaults write com.apple.dock wvous-bl-corner -int 10
+defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # bottom right: application windows
-defaults write com.apple.dock wvous-br-corner -integer 3
-defaults write com.apple.dock wvous-br-modifier -integer 0
+defaults write com.apple.dock wvous-br-corner -int 3
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 # top left: mission control
-defaults write com.apple.dock wvous-tl-corner -integer 2
-defaults write com.apple.dock wvous-tl-modifier -integer 0
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0
 
 # top right: desktop
-defaults write com.apple.dock wvous-tr-corner -integer 4
-defaults write com.apple.dock wvous-tr-modifier -integer 0
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
 
 
 # finder
-defaults write com.apple.finder ShowPathbar -boolean True
-defaults write com.apple.finder ShowStatusBar -boolean True
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
 
 # show battery % in menubar
-defaults write com.apple.menuextra.battery ShowPercent -boolean True
+defaults write com.apple.menuextra.battery ShowPercent -bool true
 
 # key repeat rate and delay
-defaults write NSGlobalDomain InitialKeyRepeat -integer 10
-defaults write NSGlobalDomain KeyRepeat -integer 2
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 2
 
 # set default text file association to vscode
 duti -s com.microsoft.vscode public.plain-text all
 
 # make tab move between "All Controls" (System Prefs -> Keyboard -> Shortcuts)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write -g AppleKeyboardUIMode -int 3
 
 # show the date in the clock
 defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm a"
 
 # use function keys as function keys
-defaults write -g com.apple.keyboard.fnState true
+defaults write -g com.apple.keyboard.fnState -bool true
 
 # change spaces shortcuts away from ctrl + <- etc.
 # todo
