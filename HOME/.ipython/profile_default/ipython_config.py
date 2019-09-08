ac80cfd246b06c4c7bd1a06291f40a059ba67ed0
Keith Devens <keith@keithdevens.com>
2019-09-08 01:39:43 -0600

    Create ipython config

---
+++ HOME/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,4 @@
+c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.highlighting_style = "monokai"
+c.TerminalInteractiveShell.term_title = False
