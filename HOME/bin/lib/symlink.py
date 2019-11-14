ec94a0bf346669afd737fa30463ba681ba46dc06
Keith Devens <keith@keithdevens.com>
2019-11-14 04:37:32 -0700

    Refactor symlink creation

--- HOME/bin/lib/symlink.py
+++ HOME/bin/lib/symlink.py
@@ -10,7 +10,6 @@ from lib.utils import read_lines_from_file
 
 log = logging.getLogger(__name__)
 
-
 class MyPath(type(Path())):  # type: ignore # https://stackoverflow.com/a/34116756
     def current_link_path(self):
         return self.__class__(os.readlink(self))
@@ -22,42 +21,54 @@ class MyPath(type(Path())):  # type: ignore # https://stackoverflow.com/a/341167
     def backup(self):
         run(['bak', self])
 
+    def walk(self):
+        """Return all (un-ignored) files under this directory"""
+        for file in self.iterdir():
+            p = self / file
+            if p.is_dir():
+                yield from p.walk()
+            else:
+                yield p
 
-def create_links(source_dir, dest_dir):
-    """Symlink all files and directories within source_dir into dest_dir."""
+
+def link_directories(source_dir, dest_dir):
+    """Symlink all files within source_dir into dest_dir."""
     source_dir = MyPath(source_dir).expanduser()
     dest_dir = MyPath(dest_dir).expanduser()
     assert source_dir != dest_dir
+    _link_directories(source_dir, dest_dir)
+
+
+def _link_directories(source_dir, dest_dir):
+    """Do the linking without validation or type coercion.
+
+    Useful as an entry-point for tests"""
     log.info(f"Creating symlinks: {source_dir} -> {dest_dir}")
-    _ready_create_links(source_dir, dest_dir)
+    for file in source_dir.walk():
+        source = source_dir / file
+        dest = dest_dir / file
+        log.debug(f"Creating link from {dest} to {source}.")
+        link_file(source, dest)
 
 
-def _ready_create_links(source_dir, dest_dir):
-    """Does the work of create_links. Assumes partials have been pre-processed
-    and user dirs expanded.
-    """
-    log.debug(f"source_dir is: {source_dir!r}, dest_dir is: {dest_dir!r}")
-    for file in source_dir.iterdir():
-        name = file.name
-        repo_path = source_dir / name
-        dest_path = dest_dir / name
-        create_link(repo_path, dest_path)
-
-
-def create_link(repo_path, dest_path):
-    log.debug(f"Creating link from {dest_path} to {repo_path}.")
-    # respect ignored files
-    if repo_path.is_ignored():
-        log.debug(f"{repo_path} is ignored.")
+def link_file(source_path, dest_path):
+    # don't link files ignored by git
+    if source_path.is_ignored():
+        log.debug(f"{source_path} is ignored.")
         return
+
+    # create parent directories if necessary
+    if not dest_path.parent.exists():
+        log.info(f"Creating directory: {dest_path.parent}")
+        dest_path.parent.mkdir(parents=True)
 
     # handle existing symlink
     if dest_path.is_symlink():
         log.debug(f"{dest_path} is an existing symlink.")
         curr_link_path = dest_path.current_link_path()
-        if curr_link_path == repo_path:
+        if curr_link_path == source_path:
             # if the link points where we want, leave it alone
-            log.debug(f"{dest_path} already points to {repo_path}, making no changes.")
+            log.debug(f"{dest_path} already points to {source_path}, making no changes.")
             return True  # return of True makes it easier to test this case
         else:
             # otherwise remove the wrong-pointing symlink
@@ -68,13 +79,5 @@ def create_link(repo_path, dest_path):
         log.info(f"Existing file at {dest_path}. Backing up.")
         dest_path.backup()
 
-    # recurse into subdirectories, ensuring they exist
-    if repo_path.is_dir():
-        if not dest_path.exists():
-            log.info(f"Creating {dest_path}")
-            dest_path.mkdir(parents=True)
-
-        return _ready_create_links(repo_path, dest_path)
-    else:  # create single symlink
-        log.info(f"Creating symlink to {repo_path} at {dest_path}")
-        dest_path.symlink_to(repo_path)
+    log.info(f"Creating symlink to {source_path} at {dest_path}")
+    dest_path.symlink_to(source_path)
--- HOME/bin/lib/tests/test_symlink.py
+++ HOME/bin/lib/tests/test_symlink.py
@@ -8,28 +8,26 @@ def MockPath(value):
     return create_autospec(symlink.MyPath, name=value, value=value)
 
 
-def test_create_links():
+def test_link_files():
     source_dir = MockPath('source')
     dest_dir = MockPath('dest')
-    source_dir.iterdir.return_value = [
-        source_dir / 'one',
-        source_dir / 'two',
-        source_dir / 'three',
-    ]
 
-    with patch('lib.symlink.create_link') as create_link:
-        symlink._ready_create_links(source_dir, dest_dir)
+    paths = [MockPath('one'), MockPath('two'), MockPath('three')]
+    paths[0].is_ignored = False
+    paths[1].is_ignored = True
+    paths[2].is_ignored = False
+
+    source_dir.walk.return_value = paths
+
+    with patch('lib.symlink.link_file') as link_file:
+        symlink._link_directories(source_dir, dest_dir)
 
     # note it's not actually checking the correct values, since mock -op- value
     # just returns an instance of the mock which compares equal to itself. not
     # sure the right way to do that with mocks, but the test is still useful for
     # now in that it checks some aspects of the calls done.
-    calls = [
-        call(source_dir / 'one', dest_dir / 'one'),
-        call(source_dir / 'two', dest_dir / 'two'),
-        call(source_dir / 'three', dest_dir / 'three'),
-    ]
-    create_link.assert_has_calls(calls)
+    link_file.assert_called_with(source_dir / paths[0], dest_dir / 'one')
+    link_file.assert_called_with(source_dir / paths[2], dest_dir / 'three')
 
 
 def test_create_symlink():
@@ -46,7 +44,7 @@ def test_create_symlink():
     dest_path.is_file.return_value = False
     dest_path.exists.return_value = False
 
-    symlink.create_link(repo_path, dest_path)
+    symlink.link_file(repo_path, dest_path)
 
     dest_path.symlink_to.assert_called_once_with(repo_path)
 
@@ -64,7 +62,7 @@ def test_create_symlink_existing_correct():
     dest_path.is_symlink.return_value = True
     dest_path.current_link_path.return_value = repo_path
 
-    returnval = symlink.create_link(repo_path, dest_path)
+    returnval = symlink.link_file(repo_path, dest_path)
 
     assert returnval == True
     dest_path.unlink.assert_not_called()
@@ -85,7 +83,7 @@ def test_create_symlink_existing_wrong():
     dest_path.exists.return_value = False
     dest_path.current_link_path.return_value = MockPath('someotherpath')
 
-    symlink.create_link(repo_path, dest_path)
+    symlink.link_file(repo_path, dest_path)
 
     dest_path.unlink.assert_called_once()
     dest_path.symlink_to.assert_called_once_with(repo_path)
@@ -99,7 +97,7 @@ def test_create_symlink_ignored():
     # an ignored file in the repo
     repo_path.is_ignored.return_value = True
 
-    symlink.create_link(repo_path, dest_path)
+    symlink.link_file(repo_path, dest_path)
 
     repo_path.is_ignored.assert_called_once()
     dest_path.unlink.assert_not_called()
@@ -111,62 +109,18 @@ def test_create_symlink_non_existent_dir():
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
 
-    # un-ignored, directory in repo
+    # un-ignored file in the repo
     repo_path.is_ignored.return_value = False
-    repo_path.is_dir.return_value = True
 
     # no file at destination
     dest_path.is_symlink.return_value = False
     dest_path.exists.return_value = False
     dest_path.is_dir.return_value = False
     dest_path.is_file.return_value = False
+    dest_path.parent.exists.return_value = False
 
-    symlink.create_link(repo_path, dest_path)
-
-    dest_path.unlink.assert_not_called()
-    dest_path.backup.assert_not_called()
-    dest_path.mkdir.assert_called_once()
-
-
-def test_create_symlink_partial_dir_existing_file():
-    repo_path = MockPath('repopath')
-    dest_path = MockPath('destpath')
-
-    # un-ignored dir in repo
-    repo_path.is_ignored.return_value = False
-    repo_path.is_dir.return_value = True
-
-    # existing file at destination
-    dest_path.is_symlink.return_value = False
-    dest_path.is_file.return_value = True
-    dest_path.exists.return_value = False
-    # is_file=True and exists=False works because of the order of things in the code
-
-    symlink.create_link(repo_path, dest_path)
-
-    dest_path.unlink.assert_not_called()
-    dest_path.backup.assert_called_once()
-    dest_path.mkdir.assert_called_once()
-    dest_path.symlink_to.assert_not_called()
-
-
-def test_create_symlink_partial_dir_existing_dir():
-    repo_path = MockPath('repopath')
-    dest_path = MockPath('destpath')
-
-    # un-ignored dir in repo
-    repo_path.is_ignored.return_value = False
-    repo_path.is_dir.return_value = True
-
-    # existing dir at dest
-    dest_path.is_symlink.return_value = False
-    dest_path.is_file.return_value = False
-    dest_path.exists.return_value = True
-    dest_path.is_dir.return_value = True
-
-    symlink.create_link(repo_path, dest_path)
+    symlink.link_file(repo_path, dest_path)
 
     dest_path.unlink.assert_not_called()
     dest_path.backup.assert_not_called()
-    dest_path.mkdir.assert_not_called()
-    dest_path.symlink_to.assert_not_called()
+    dest_path.parent.mkdir.assert_called_once()
