ac538aa88ef5e3af1dd2ff741b3467255077b766
Keith Devens <keith@keithdevens.com>
2019-11-14 02:48:09 -0700

    Remove partials, only files get symlinked

--- HOME/bin/lib/symlink.py
+++ HOME/bin/lib/symlink.py
@@ -20,86 +20,19 @@ class MyPath(type(Path())):  # type: ignore # https://stackoverflow.com/a/341167
         return run(['git', 'check-ignore', '-q', str(self)]).returncode == 0
 
     def backup(self):
-        ts = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
-        suffix = '.bak.' + ts
-        i = 1
-        while True:  # keep adding to filename until a path is free
-            backup_path = self.with_suffix(self.suffix + suffix * i)
-            if not backup_path.exists():
-                break
-            i += 1
-
-        self.rename(backup_path)
-        return backup_path
+        run(['bak', self])
 
 
-class Partials:
-    def __init__(self, path):
-        self.path = path
-        self.source = read_lines_from_file(path)
-        self.process()  # assigns self.paths
-
-    def __contains__(self, value):
-        return value in self.paths
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({', '.join(self.source)})"
-
-    def add(self, path):
-        log.info(f"Adding {str(path)!r} to partials")
-        self.source = sorted(set(self.source + [str(path)]))  # ensure no dupes
-        self.process()
-
-    def save(self):
-        log.info(f"Writing partials to {str(self.path)!r}")
-        with open(self.path, 'w') as f:
-            f.writelines(f"{line}\n" for line in self.source)
-
-    def process(self):
-        self.paths = self._process(self.source)
-
-    @staticmethod
-    def _process(partial_paths):
-        """Expand partials as-specified-in-the-config to what we need in code.
-
-        Background: The difference between a "partial" directory and a regular
-        one is that a regular directory is symlinked, whereas only the
-        *contents* of a partial directory are symlinked. A regular directory is
-        under full contol of setup, while a partial directory is only partially
-        under control of setup. For a partial directory, the directory as a
-        whole is not under source control, only specific files within it.
-
-        Unfortunately, it can't be obvious when a directory is in source control
-        under (eg) HOME/a/b/c where we want to "start source controlling". It
-        could be a, b, or c. It's something that needs to be configured.
-
-        ---
-
-        The config specifies a full absolute path, ~/a/b/c. Having a
-        sub-directory that is a partial logically implies that directories above
-        it are partials as well. So this expands the list of partial directories
-        into the set of all parent directories up to and including the partial
-        directory. That way it's a simple set membership test to determine
-        whether a given path is a partial.
-        """
-        original_partials = {MyPath(p).expanduser() for p in partial_paths}
-        all_partials = original_partials.copy()
-        for p in original_partials:
-            all_partials |= set(p.parents)
-
-        return all_partials
-
-
-def create_links(source_dir, dest_dir, partials):
+def create_links(source_dir, dest_dir):
     """Symlink all files and directories within source_dir into dest_dir."""
     source_dir = MyPath(source_dir).expanduser()
     dest_dir = MyPath(dest_dir).expanduser()
     assert source_dir != dest_dir
     log.info(f"Creating symlinks: {source_dir} -> {dest_dir}")
-    _ready_create_links(source_dir, dest_dir, partials)
+    _ready_create_links(source_dir, dest_dir)
 
 
-def _ready_create_links(source_dir, dest_dir, partials):
+def _ready_create_links(source_dir, dest_dir):
     """Does the work of create_links. Assumes partials have been pre-processed
     and user dirs expanded.
     """
@@ -108,10 +41,10 @@ def _ready_create_links(source_dir, dest_dir, partials):
         name = file.name
         repo_path = source_dir / name
         dest_path = dest_dir / name
-        create_link(repo_path, dest_path, partials)
+        create_link(repo_path, dest_path)
 
 
-def create_link(repo_path, dest_path, partials: Container[Path]):
+def create_link(repo_path, dest_path):
     log.debug(f"Creating link from {dest_path} to {repo_path}.")
     # respect ignored files
     if repo_path.is_ignored():
@@ -130,32 +63,18 @@ def create_link(repo_path, dest_path, partials: Container[Path]):
             # otherwise remove the wrong-pointing symlink
             log.info(f"{dest_path} points to {curr_link_path}. Removing.")
             dest_path.unlink()
+    # back up existing files
+    elif dest_path.is_file():
+        log.info(f"Existing file at {dest_path}. Backing up.")
+        dest_path.backup()
 
-    exists_msg = f"Existing file at {dest_path}. Backing up."
-    # handle partial directories
-    if dest_path in partials:
-        if not repo_path.is_dir():
-            # sanity check
-            msg = f"Error in config: {dest_path} is specified as a partial but {repo_path} is a file"
-            log.error(msg)
-            raise Exception(msg)
-
-        log.debug(f"{dest_path} is a partial directory")
-        # if it's a file, back it up. Otherwise, ensure the dir exists
-        if dest_path.is_file():
-            log.info(exists_msg)
-            dest_path.backup()
-
+    # recurse into subdirectories, ensuring they exist
+    if repo_path.is_dir():
         if not dest_path.exists():
             log.info(f"Creating {dest_path}")
             dest_path.mkdir(parents=True)
 
-        return _ready_create_links(repo_path, dest_path, partials)
-
-    # everything else is a file or dir that should be symlinked
-    if dest_path.exists():
-        log.info(exists_msg)
-        dest_path.backup()
-
-    log.info(f"Creating symlink to {repo_path} at {dest_path}")
-    dest_path.symlink_to(repo_path)
+        return _ready_create_links(repo_path, dest_path)
+    else:  # create single symlink
+        log.info(f"Creating symlink to {repo_path} at {dest_path}")
+        dest_path.symlink_to(repo_path)
--- HOME/bin/lib/tests/test_symlink.py
+++ HOME/bin/lib/tests/test_symlink.py
@@ -11,7 +11,6 @@ def MockPath(value):
 def test_create_links():
     source_dir = MockPath('source')
     dest_dir = MockPath('dest')
-    partials = {'one'}
     source_dir.iterdir.return_value = [
         source_dir / 'one',
         source_dir / 'two',
@@ -19,41 +18,53 @@ def test_create_links():
     ]
 
     with patch('lib.symlink.create_link') as create_link:
-        symlink._ready_create_links(source_dir, dest_dir, partials)
+        symlink._ready_create_links(source_dir, dest_dir)
 
     # note it's not actually checking the correct values, since mock -op- value
     # just returns an instance of the mock which compares equal to itself. not
     # sure the right way to do that with mocks, but the test is still useful for
     # now in that it checks some aspects of the calls done.
     calls = [
-        call(source_dir / 'one', dest_dir / 'one', partials),
-        call(source_dir / 'two', dest_dir / 'two', partials),
-        call(source_dir / 'three', dest_dir / 'three', partials),
+        call(source_dir / 'one', dest_dir / 'one'),
+        call(source_dir / 'two', dest_dir / 'two'),
+        call(source_dir / 'three', dest_dir / 'three'),
     ]
     create_link.assert_has_calls(calls)
 
 
 def test_create_symlink():
-    "Test that create_symlink correctly calls os.symlink"
+    "create_symlink should call os.symlink"
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # an un-ignored file in the repo
     repo_path.is_ignored.return_value = False
+    repo_path.is_dir.return_value = False
+
+    # no file at destination
     dest_path.is_symlink.return_value = False
+    dest_path.is_file.return_value = False
     dest_path.exists.return_value = False
 
-    symlink.create_link(repo_path, dest_path, partials={})
+    symlink.create_link(repo_path, dest_path)
 
     dest_path.symlink_to.assert_called_once_with(repo_path)
 
 
 def test_create_symlink_existing_correct():
+    "An existing, correct, symlink should not be changed"
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # an un-ignored file in the repo
     repo_path.is_ignored.return_value = False
+    repo_path.is_dir.return_value = False
+
+    # an existing symlink at destination
     dest_path.is_symlink.return_value = True
     dest_path.current_link_path.return_value = repo_path
 
-    returnval = symlink.create_link(repo_path, dest_path, partials={})
+    returnval = symlink.create_link(repo_path, dest_path)
 
     assert returnval == True
     dest_path.unlink.assert_not_called()
@@ -61,58 +72,77 @@ def test_create_symlink_existing_correct():
 
 
 def test_create_symlink_existing_wrong():
+    "An existing, incorrect, symlink should be changed"
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # an un-ignored file in the repo
     repo_path.is_ignored.return_value = False
+    repo_path.is_dir.return_value = False
+
+    # an existing symlink at the destination
     dest_path.is_symlink.return_value = True
     dest_path.exists.return_value = False
     dest_path.current_link_path.return_value = MockPath('someotherpath')
 
-    symlink.create_link(repo_path, dest_path, partials={})
+    symlink.create_link(repo_path, dest_path)
 
     dest_path.unlink.assert_called_once()
     dest_path.symlink_to.assert_called_once_with(repo_path)
 
 
 def test_create_symlink_ignored():
+    "A file ignored in git should not be symlinked"
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # an ignored file in the repo
     repo_path.is_ignored.return_value = True
 
-    symlink.create_link(repo_path, dest_path, partials={})
+    symlink.create_link(repo_path, dest_path)
 
     repo_path.is_ignored.assert_called_once()
     dest_path.unlink.assert_not_called()
     dest_path.symlink_to.assert_not_called()
 
 
-def test_create_symlink_existing_dir():
+def test_create_symlink_non_existent_dir():
+    "A directory at the destination should be created if it doesn't exist"
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # un-ignored, directory in repo
     repo_path.is_ignored.return_value = False
     repo_path.is_dir.return_value = True
-    dest_path.is_symlink.return_value = False
-    dest_path.is_dir.return_value = True
 
-    symlink.create_link(repo_path, dest_path, partials={})
+    # no file at destination
+    dest_path.is_symlink.return_value = False
+    dest_path.exists.return_value = False
+    dest_path.is_dir.return_value = False
+    dest_path.is_file.return_value = False
+
+    symlink.create_link(repo_path, dest_path)
 
     dest_path.unlink.assert_not_called()
-    dest_path.backup.assert_called_once()
-    dest_path.symlink_to.assert_called_once_with(repo_path)
+    dest_path.backup.assert_not_called()
+    dest_path.mkdir.assert_called_once()
 
 
 def test_create_symlink_partial_dir_existing_file():
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # un-ignored dir in repo
     repo_path.is_ignored.return_value = False
     repo_path.is_dir.return_value = True
+
+    # existing file at destination
     dest_path.is_symlink.return_value = False
     dest_path.is_file.return_value = True
     dest_path.exists.return_value = False
     # is_file=True and exists=False works because of the order of things in the code
 
-    partials = {MockPath('otherpath'), dest_path}
-    symlink.create_link(repo_path, dest_path, partials=partials)
+    symlink.create_link(repo_path, dest_path)
 
     dest_path.unlink.assert_not_called()
     dest_path.backup.assert_called_once()
@@ -123,35 +153,18 @@ def test_create_symlink_partial_dir_existing_file():
 def test_create_symlink_partial_dir_existing_dir():
     repo_path = MockPath('repopath')
     dest_path = MockPath('destpath')
+
+    # un-ignored dir in repo
     repo_path.is_ignored.return_value = False
     repo_path.is_dir.return_value = True
+
+    # existing dir at dest
     dest_path.is_symlink.return_value = False
     dest_path.is_file.return_value = False
     dest_path.exists.return_value = True
     dest_path.is_dir.return_value = True
 
-    partials = {MockPath('otherpath'), dest_path}
-    symlink.create_link(repo_path, dest_path, partials=partials)
-
-    dest_path.unlink.assert_not_called()
-    dest_path.backup.assert_not_called()
-    dest_path.mkdir.assert_not_called()
-    dest_path.symlink_to.assert_not_called()
-
-
-def test_create_symlink_partial_file():
-    repo_path = MockPath('repopath')
-    dest_path = MockPath('destpath')
-    repo_path.is_ignored.return_value = False
-    repo_path.is_dir.return_value = False
-    repo_path.is_file.return_value = True
-    dest_path.is_symlink.return_value = False
-    dest_path.is_file.return_value = True
-
-    partials = {MockPath('otherpath'), dest_path}
-
-    with pytest.raises(Exception):
-        symlink.create_link(repo_path, dest_path, partials=partials)
+    symlink.create_link(repo_path, dest_path)
 
     dest_path.unlink.assert_not_called()
     dest_path.backup.assert_not_called()
