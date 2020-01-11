kbd/setup

commit 0b3625231a444b2927c6bf9c01fbf3ba081db48e

Change read_lines_from_file to read_config_file
The name better describes what the function does.
Accordingly, comment now has a default value since you want
comments on by default for config files. Update tests to match.

HOME/bin/lib/setup/packages.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from lib import homebrew, setup
 from lib.mac import defaults
-from lib.utils import run
+from lib.utils import run, read_config_file
 
 log = logging.getLogger()
 
@@ -62,10 +62,10 @@ def vscode(settings):
     config_path = settings['extensions']
     log.info("Updating Visual Studio Code extensions")
 
-    # show currently installed extensions
+    # get installed/expected extensions
     cmd = ['code', '--list-extensions']
     current_extensions = set(map(str.strip, run(cmd, cap='stdout').splitlines()))
-    expected_extensions = set(map(str.strip, open(config_path)))
+    expected_extensions = read_config_file(config_path)
 
     fmt = lambda s: ', '.join(sorted(s, key=str.lower))
 

HOME/bin/clip
@@ -2,10 +2,10 @@
 import sys
 from pathlib import Path
 
-from lib.utils import run, read_lines_from_file
+from lib.utils import run, read_config_file
 
 CLIPS_PATH = Path(__file__).parent / 'data' / 'clips.txt'
-CLIPS = dict(line.split(maxsplit=1) for line in read_lines_from_file(CLIPS_PATH))
+CLIPS = dict(line.split(maxsplit=1) for line in read_config_file(CLIPS_PATH))
 
 def print_clips(CLIPS):
     print("Available clips:")

HOME/bin/lib/tests/test_utils.py
@@ -77,20 +77,31 @@ def test_run_shell_input():
     assert output == expected_output
 
 
-def test_read_lines_from_file():
-    values = ['one', 'two', 'three']
+def test_read_config():
+    values = ['one', 'two', 'three', '#four', '     \t  ', 'five']
     with tempfile.NamedTemporaryFile() as t:
         t.writelines(f'{v}\n'.encode() for v in values)
         t.flush()
-        actual = utils.read_lines_from_file(t.name)
+        actual = utils.read_config_file(t.name)
 
-    assert actual == values
+    expected = ['one', 'two', 'three', 'five']
+    assert actual == expected
 
-def test_read_lines_from_file_with_blanks_and_comments():
+def test_read_config_no_comments():
+    values = ['one', 'two', 'three', '#four', '     \t  ', 'five']
+    with tempfile.NamedTemporaryFile() as t:
+        t.writelines(f'{v}\n'.encode() for v in values)
+        t.flush()
+        actual = utils.read_config_file(t.name, comment=None)
+
+    expected = ['one', 'two', 'three', '#four', 'five']
+    assert actual == expected
+
+def test_read_config_with_blanks_and_comments():
     values = ['one', 'two', 'three', '# commented line', 'four', ' ', 'six']
     with tempfile.NamedTemporaryFile() as t:
         t.writelines(f'{v}\n'.encode() for v in values)
         t.flush()
-        actual = utils.read_lines_from_file(t.name, comment='#')
+        actual = utils.read_config_file(t.name, comment='#')
 
     assert actual == ['one', 'two', 'three', 'four', 'six']

HOME/bin/lib/utils.py
@@ -40,15 +40,13 @@ def partition_by_regex(regex, list):
     return partition(r.search, list)
 
 
-def read_lines_from_file(path, comment=None):
-    """Read the lines from a file, skipping empty lines and (optionally) commented lines.
+def read_config_file(path, comment='#'):
+    """Read the lines from a file, skipping empty and commented lines.
 
-    If 'comment' string is provided, lines beginning with 'comment' will not be returned.
+    Don't process comments if 'comment' is falsy.
     """
     with open(path) as file:
-        lines = [line.rstrip() for line in file]
-
-    return [
-        line for line in lines
-        if line and not (comment and line.startswith(comment))
-    ]
+        return [
+            line for line in (line.rstrip() for line in file)
+            if line and not (comment and line.startswith(comment))
+        ]

commit 1bf6006da2f21e541a5bcac8d7848ff3f9eeaeef

Fix bug where expected_extensions was not converted to set
Introduced in 0b36252

HOME/bin/lib/setup/packages.py
@@ -65,7 +65,7 @@ def vscode(settings):
     # get installed/expected extensions
     cmd = ['code', '--list-extensions']
     current_extensions = set(map(str.strip, run(cmd, cap='stdout').splitlines()))
-    expected_extensions = read_config_file(config_path)
+    expected_extensions = set(read_config_file(config_path))
 
     fmt = lambda s: ', '.join(sorted(s, key=str.lower))
 