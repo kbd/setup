02ab421105754e6ec258bc7c48b794bcb8ad95ec
Keith Devens <keith@keithdevens.com>
2020-08-22 13:24:07 -0600

    Fix spammy logging during IPython tab complete
    https://github.com/ipython/ipython/issues/10946

--- HOME/.ipython/profile_default/ipython_config.py
+++ HOME/.ipython/profile_default/ipython_config.py
@@ -2,3 +2,5 @@ c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.highlighting_style = "monokai"
 c.TerminalInteractiveShell.term_title = False
+import logging
+logging.getLogger('parso').level = logging.WARN
