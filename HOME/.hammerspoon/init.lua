af1bf27b0792184ef5a365abeaadbc70f90e3986
Keith Devens <keith@keithdevens.com>
2021-02-03 22:28:20 -0700

    Implement "hyper" key in Karabiner & basic movement in Hammerspoon

--- HOME/.hammerspoon/init.lua
+++ HOME/.hammerspoon/init.lua
@@ -1,5 +1,26 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+hs.alert.show("Hammerspoon config loaded")
+
+hyper = {"cmd", "alt", "ctrl", "shift"}
+
+hs.hotkey.bind(hyper, "R", function()
   hs.reload()
 end)
 
-hs.alert.show("Hammerspoon config loaded")
+function move(direction, increment)
+  return function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    f[direction] = f[direction] + increment
+    win:setFrame(f)
+  end
+end
+
+right = move("x", 10)
+left = move("x", -10)
+up = move("y", -10)
+down = move("y", 10)
+
+hs.hotkey.bind(hyper, "Right", right, nil, right)
+hs.hotkey.bind(hyper, "Left", left, nil, left)
+hs.hotkey.bind(hyper, "Up", up, nil, up)
+hs.hotkey.bind(hyper, "Down", down, nil, down)
