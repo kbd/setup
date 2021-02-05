ab7d1fe0b6fc80fba8db04a1a4809f8cfa11f0a0
Keith Devens <keith@keithdevens.com>
2021-02-04 17:20:15 -0700

    Add hyper-key application shortcuts

--- HOME/.hammerspoon/init.lua
+++ HOME/.hammerspoon/init.lua
@@ -1,9 +1,23 @@
 hs.alert.show("Hammerspoon config loaded")
 
+browser = "Google Chrome"
+editor = "Visual Studio Code"
+terminal = "Kitty"
+
 hyper = {"cmd", "alt", "ctrl", "shift"}
 
 hs.hotkey.bind(hyper, "R", function()
   hs.reload()
+end)
+
+hs.hotkey.bind(hyper, "B", function()
+  hs.application.launchOrFocus(browser)
+end)
+hs.hotkey.bind(hyper, "E", function()
+  hs.application.launchOrFocus(editor)
+end)
+hs.hotkey.bind(hyper, "T", function()
+  hs.application.launchOrFocus(terminal)
 end)
 
 function move(axis, increment)
