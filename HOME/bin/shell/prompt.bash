edb0d0b027c091f480788b043f306620d771ec9b
Keith Devens <keith@keithdevens.com>
2021-03-01 11:29:25 -0700

    Remove colors.sh
    Zsh has easy access to colors built-in with:
    
        $ autoload colors && colors
    
    which gives you colors in $fg and $bg, like:
    
        echo "$fg[red]hello${reset_color}world"
    
    Since loading colors on startup is unnecessary, this shaves
    off a lot from shell startup time. Move color definitions
    into prompt.bash, which is the only thing still using them.

--- HOME/bin/shell/prompt.bash
+++ HOME/bin/shell/prompt.bash
@@ -18,7 +18,42 @@
 # $PROMPT_BARE
 #   set to enable a very minimal prompt, useful for copying exmaples
 #
-# note: this code depends on colors.sh
+
+# http://linuxcommand.org/lc3_adv_tput.php
+# http://unix.stackexchange.com/questions/269077/tput-setaf-color-table-how-to-determine-color-codes
+# http://misc.flogisoft.com/bash/tip_colors_and_formatting
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+typeset -A COL  # declare associative array, works both in bash and zsh
+export COL
+
+COL[reset]="$(tput sgr0)"
+COL[bold]="$(tput bold)"
+COL[dim]="$(tput dim)"
+COL[uline]="$(tput smul)"
+COL[no_uline]="$(tput rmul)"
+COL[blink]="$(tput blink)"
+
+COL[black]="$(tput setaf 0)"
+COL[red]="$(tput setaf 1)"
+COL[green]="$(tput setaf 2)"
+COL[yellow]="$(tput setaf 3)"
+COL[blue]="$(tput setaf 4)"
+COL[magenta]="$(tput setaf 5)"
+COL[cyan]="$(tput setaf 6)"
+COL[white]="$(tput setaf 7)"
+# setaf 9 is wrong on mac, but right in screen and tmux ¯\_(ツ)_/¯
+COL[default]="$(tput setaf 9)"
+
+COL[b_black]="$(tput setab 0)"
+COL[b_red]="$(tput setab 1)"
+COL[b_green]="$(tput setab 2)"
+COL[b_yellow]="$(tput setab 3)"
+COL[b_blue]="$(tput setab 4)"
+COL[b_magenta]="$(tput setab 5)"
+COL[b_cyan]="$(tput setab 6)"
+COL[b_white]="$(tput setab 7)"
+# see comment for setaf 9
+COL[b_default]="$(tput setab 9)"
 
 filter() {
   echo "$1" | tr ' ' '\n' | grep -Ewv "$2" | tr '\n' ' '
