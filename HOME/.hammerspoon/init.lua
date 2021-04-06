0c61e7ccffd96b276d7e86bb43aab1693a741a32
Keith Devens <keith@keithdevens.com>
2021-04-06 01:06:59 -0600

    Improve audio fuzzy selector

--- HOME/.hammerspoon/init.lua
+++ HOME/.hammerspoon/init.lua
@@ -65,6 +65,9 @@ end
 function fuzzy(choices, func)
   local chooser = hs.chooser.new(func)
   chooser:choices(choices)
+  chooser:searchSubText(true)
+  chooser:fgColor({hex="#bbf"})
+  chooser:subTextColor({hex="#aaa"})
   chooser:width(25)
   chooser:show()
 end
@@ -72,17 +75,32 @@ end
 function showAudioFuzzy()
   local devices = hs.audiodevice.allDevices()
   local choices = {}
+  local active_input = hs.audiodevice.defaultInputDevice()
+  local active_output = hs.audiodevice.defaultOutputDevice()
+  local active, subtext
   for i=1, #devices do
+    if devices[i]:isOutputDevice() then
+      active = devices[i]:uid() == active_output:uid()
+      subtext = "output"
+    else
+      active = devices[i]:uid() ~= active_input:uid()
+      subtext = "input"
+    end
+    if active then subtext = subtext .. " (active)" end
     choices[i] = {
       text = devices[i]:name(),
       uid = devices[i]:uid(),
-      subText = devices[i]:isOutputDevice() and "output" or "input"
+      subText = subtext,
+      valid = not active,
     }
   end
   fuzzy(choices, selectAudio)
 end
 
 function selectAudio(audio)
+  if audio == nil then -- nothing selected
+    return
+  end
   device = hs.audiodevice.findDeviceByUID(audio.uid)
   hs.alert.show("Setting "..audio.subText.." device: "..device:name())
   if device:isOutputDevice() then
