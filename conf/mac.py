46dd8ae650fce96f363ad052034fe861d62be119
Keith Devens <keith@keithdevens.com>
2021-04-21 13:27:34 -0600

    Remove Mac settings related to Caffeine

--- conf/mac.py
+++ conf/mac.py
@@ -114,12 +114,8 @@ dash = defaults['com.kapeli.dashdoc']
 dash['syncFolderPath'] = "~/Documents/Dash"
 dash['snippetSQLPath'] = "~/Documents/Dash/snippets.dash"
 
-caffeine = defaults['com.intelliscapesolutions.caffeine']
-caffeine['ActivateOnLaunch'] = False
-caffeine['SuppressLaunchMessage'] = True
-
 # startup items - https://apple.stackexchange.com/a/310502/
-required_login_apps = {'SpotMenu', 'Flux', 'Alfred 4', 'Horo', 'Caffeine', 'Hammerspoon'}
+required_login_apps = {'SpotMenu', 'Flux', 'Alfred 4', 'Horo', 'Hammerspoon'}
 current_login_apps = set(
     filter(None,
         run(['osascript', '-e' 'tell application "System Events" to get the name of every login item'], cap='stdout').strip().split(', ')
