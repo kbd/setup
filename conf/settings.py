9aa47e8731d6ba2823075c1a7598bb066134476b
Keith Devens <keith@keithdevens.com>
2021-10-15 22:21:24 -0600

    setup: simplify config by making 'cmd' the default

--- conf/settings.py
+++ conf/settings.py
@@ -1,58 +1,42 @@
 {
-    'brew': {
-        'cmd': (
-            ['homebrew-workflow', 'Brewfile'],
-            # set shell to homebrew'd shell
-            "update_shell.sh `brew --prefix`/bin/zsh",
-            # install fzf
-            "$(brew --prefix)/opt/fzf/install --key-bindings --completion --no-update-rc --xdg",
-            # patch fzf's history format to include timestamp
-            'perl -pi -e \'s/fc -rl 1/fc -rli 1/\' "$(brew --prefix fzf)/shell/key-bindings.zsh"',
-            # https://docs.docker.com/docker-for-mac/#zsh
-            """
-                etc=/Applications/Docker.app/Contents/Resources/etc;
-                sf=`brew --prefix`/share/zsh/site-functions;
-                ln -sf $etc/docker.zsh-completion $sf/_docker;
-                ln -sf $etc/docker-compose.zsh-completion $sf/_docker-compose;
-            """,
-        ),
-    },
-    'python': {
-        'cmd': (
-            ['pip3', 'install', '--upgrade', '-r', 'requirements.txt'],
-            'poetry completions zsh > `brew --prefix`/share/zsh/site-functions/_poetry',
-        ),
-    },
-    'pipx': {
-        'cmd': "st <(cat pipx.txt) '-' <(pipx list --json | jq -r '.venvs | keys[]') | xargs -t pipx install"
-    },
-    'node': {
-        'cmd': "cat npm.txt | xargs -t npm install -g",
-    },
-    'go': {
-        'cmd': "cat go.txt | xargs -t -L1 go get -v -u"
-    },
-    'nim': {
-        'cmd': "cat nimble.txt | xargs -to nimble install"
-    },
-    'rust': {
-        'cmd': """
-            rustup-init -y --no-modify-path;
-            source $HOME/.cargo/env;
-            rustup update;
-            rustup install nightly;
+    'brew': (
+        ['homebrew-workflow', 'Brewfile'],
+        # set shell to homebrew'd shell
+        "update_shell.sh `brew --prefix`/bin/zsh",
+        # install fzf
+        "$(brew --prefix)/opt/fzf/install --key-bindings --completion --no-update-rc --xdg",
+        # patch fzf's history format to include timestamp
+        'perl -pi -e \'s/fc -rl 1/fc -rli 1/\' "$(brew --prefix fzf)/shell/key-bindings.zsh"',
+        # https://docs.docker.com/docker-for-mac/#zsh
+        """
+            etc=/Applications/Docker.app/Contents/Resources/etc;
+            sf=`brew --prefix`/share/zsh/site-functions;
+            ln -sf $etc/docker.zsh-completion $sf/_docker;
+            ln -sf $etc/docker-compose.zsh-completion $sf/_docker-compose;
         """,
-    },
-    'cargo': {
-        'cmd': (
-            "cat cargo.txt | xargs -t cargo install",
-            # create directory in case bootstrapping when symlinks not yet created
-            """
-                mkdir -p ~/bin/shell/3rdparty/;
-                broot --set-install-state refused --print-shell-function zsh > ~/bin/shell/3rdparty/br.zsh;
-            """,
-        )
-    },
+    ),
+    'python': (
+        ['pip3', 'install', '--upgrade', '-r', 'requirements.txt'],
+        'poetry completions zsh > `brew --prefix`/share/zsh/site-functions/_poetry',
+    ),
+    'pipx': "st <(cat pipx.txt) '-' <(pipx list --json | jq -r '.venvs | keys[]') | xargs -t pipx install",
+    'node': "cat npm.txt | xargs -t npm install -g",
+    'go': "cat go.txt | xargs -t -L1 go get -v -u",
+    'nim': "cat nimble.txt | xargs -to nimble install",
+    'rust': """
+        rustup-init -y --no-modify-path;
+        source $HOME/.cargo/env;
+        rustup update;
+        rustup install nightly;
+    """,
+    'cargo': (
+        "cat cargo.txt | xargs -t cargo install",
+        # create directory in case bootstrapping when symlinks not yet created
+        """
+            mkdir -p ~/bin/shell/3rdparty/;
+            broot --set-install-state refused --print-shell-function zsh > ~/bin/shell/3rdparty/br.zsh;
+        """,
+    ),
     'mac': {
         'exec': 'import runpy; runpy.run_path("mac.py")'
     },
@@ -60,9 +44,7 @@
         'help': "Restart Finder, Menubar, Dock, etc.",
         'exec': 'import lib.mac; lib.mac.restart_os_functions()'
     },
-    'vscode': {
-        'cmd': ['install-vscode-extensions', 'vscode.txt'],
-    },
+    'vscode': ['install-vscode-extensions', 'vscode.txt'],
     'debug': {
         'help': "Start an interactive console",
         'exec': 'import code; code.interact(local=globals())',
@@ -71,9 +53,7 @@
         'help': "Open the setup directory in your editor",
         'cmd': ['bash', '-ic', 'edit .']
     },
-    'manual': {
-        'cmd': ['install-manual', 'manual.toml', VENDOR],
-    },
+    'manual': ['install-manual', 'manual.toml', VENDOR],
     'symlinks': {
         'exec': "run_commands(['symgr', *debug_if_debug(), HOME, Path.home()])"
     },
