760f33c6b0717be3dfb8d8e2931a5cfaef45f8b3
Keith Devens <keith@keithdevens.com>
2022-02-06 15:41:06 -0700

    ipython: disable black autoformatting
    Bad feature, and seems to be buggy with top-level await

--- HOME/.ipython/profile_default/ipython_config.py
+++ HOME/.ipython/profile_default/ipython_config.py
@@ -2,5 +2,8 @@ c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.highlighting_style = "monokai"
 c.TerminalInteractiveShell.term_title = False
+c.TerminalInteractiveShell.autoformatter = None
+
 import logging
+
 logging.getLogger('parso').level = logging.WARN
