58b7f2348e5531a33801448fa96f0b1d3aec5b69
Keith Devens <keith@keithdevens.com>
2022-04-17 20:31:06 -0600

    kitty-tab-color: golf lines

--- HOME/bin/kitty-tab-color.cr
+++ HOME/bin/kitty-tab-color.cr
@@ -9,19 +9,15 @@ if ARGV[0]? == "-s"
 else
   # pull colors from vscode settings if exists
   file = ".vscode/settings.json"
-  if !File.exists?(file)
-    exit 0
-  end
+  exit 0 if !File.exists?(file)
 
   json = JSON.parse(File.read(file))
-  color = json["peacock.color"]?
-  if color
-    result = "
+  if color = json["peacock.color"]?
+    puts "
 export KITTY_TAB_AFG=#fff
 export KITTY_TAB_ABG=#{color}
 export KITTY_TAB_IFG=#ccc
 export KITTY_TAB_IBG=#{color}
 "
-    puts result
   end
 end
