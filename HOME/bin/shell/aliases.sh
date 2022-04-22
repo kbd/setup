b94e2e5f7078fd48d734e08b1440c304c7037680
Keith Devens <keith@keithdevens.com>
2022-04-21 21:32:18 -0600

    Reorganize aliases

--- HOME/bin/shell/aliases.sh
+++ HOME/bin/shell/aliases.sh
@@ -1,43 +1,16 @@
 #!/usr/bin/env bash
 
-# VARS
+# path/system
 if [[ -z "$PATH_SET" ]]; then
   export PATH="$HOME/bin:$PATH:$HOME/.cargo/bin:$HOME/go/bin:$HOME/.local/bin:$HOME/.nimble/bin"
   export PATH_SET=1
 fi
-export PLATFORM="$(uname)"
 export XDG_CONFIG_HOME=~/.config
-export PAGER=less
-export LESS='-iMFx4 --mouse --incsearch' # smart-case, status bar, quit 1 screen, 4sp tabs
-export DELTA_PAGER="less $LESS -R"
-export VISUAL='code -nw'
-export EDITOR=vim
-export GIT_EDITOR='kw --wait vim'
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export PYTHONDONTWRITEBYTECODE=1
-export PYTHONBREAKPOINT=pudb.set_trace
-export PTPYTHON_CONFIG_HOME=$XDG_CONFIG_HOME/ptpython  # defaults to ~/Library/Application Support/... on Mac
-export PIPENV_SHELL_FANCY=1
-export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSTALL_CLEANUP=1
-export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
-export ERL_AFLAGS="-kernel shell_history enabled" # remember Elixir iex history across sessions
-export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/ripgrep/rc
-
-# fzf
-export FZF_DEFAULT_COMMAND='fd -tf -HL'
-export FZF_DEFAULT_OPTS='--height 30% --reverse --multi'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND='fd -td -HL'
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-_fzf_compgen_path() { fd -tf -HL . "$1"; }
-_fzf_compgen_dir() { fd -td -HL . "$1"; }
 
 # PLATFORM SPECIFIC
-if [[ $PLATFORM == 'Darwin' ]]; then
+if [[ "$(uname)" == 'Darwin' ]]; then
   # PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
   # MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
@@ -45,7 +18,7 @@ if [[ $PLATFORM == 'Darwin' ]]; then
   alias awk=gawk
   alias sed=gsed
   alias tar=gtar
-  ls_path='/usr/local/bin/gls'
+  LS_PATH='/usr/local/bin/gls'
 
   alias lock='pmset displaysleepnow'
   alias locks='pmset sleepnow' # locks = "lock+sleep". 'sleep' is a unix command
@@ -56,6 +29,8 @@ fi
 
 # SHELL SPECIFIC
 if [[ $ZSH_VERSION ]]; then
+  export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+
   # global aliases
   alias -g L='| $PAGER'
   alias -g H='| head'
@@ -71,6 +46,33 @@ if [[ $TERM == 'xterm-kitty' ]]; then
   alias cati="kitty +kitten icat --align=left"
 fi
 
+# pag{ers,ing}/editors
+export PAGER=less
+export LESS='-iMFx4 --mouse --incsearch' # smart-case, status bar, quit 1 screen, 4sp tabs
+export EDITOR=vim
+export VISUAL='code -nw'
+export GIT_EDITOR='kw --wait vim'
+export DELTA_PAGER="less $LESS -R"
+alias http='autopager http --pretty=all'
+alias https='autopager https --pretty=all'
+alias jq='autopager jq -C'
+alias curl='autopager curl -L'
+alias xh='autopager xh --pretty=all'
+alias yq='autopager yq -C'
+alias gh='PAGER= gh' # use gh default pager; gh needs 'less -R' for colors
+alias glab='PAGER= glab' # ...
+
+# edit/open
+alias edit=code
+alias e=edit
+alias e.='e .'
+alias e-='e -'
+alias eg='e -g'
+alias o=open
+alias o.='o .'
+alias a='o -a'
+te(){ t "$@" && e "$@"; }
+
 # directory/navigation
 alias   -- -='cd -'
 alias  -- --='cd -2'
@@ -81,7 +83,7 @@ alias   ....='cd ../../..'
 alias  .....='cd ../../../..'
 alias ......='cd ../../../../..'
 # shellcheck disable=SC2139
-alias ls="${ls_path:-ls} -F --color=auto --group-directories-first"
+alias ls="${LS_PATH:-ls} -F --color=auto --group-directories-first"
 alias l=ls
 alias la='ls -A'
 alias lt='ls -t'
@@ -103,16 +105,14 @@ mcd() {
   mkdir -p -- "$1" && cl "$@" -A
 }
 
-# edit/open
-alias edit=code
-alias e=edit
-alias e.='e .'
-alias e-='e -'
-alias eg='e -g'
-alias o=open
-alias o.='o .'
-alias a='o -a'
-te(){ t "$@" && e "$@"; }
+# fzf
+export FZF_DEFAULT_COMMAND='fd -tf -HL'
+export FZF_DEFAULT_OPTS='--height 30% --reverse --multi'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND='fd -td -HL'
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+_fzf_compgen_path() { fd -tf -HL . "$1"; }
+_fzf_compgen_dir() { fd -td -HL . "$1"; }
 
 # git
 # create aliases for all short (<= 4 character) git aliases
@@ -134,21 +134,7 @@ gccb() {
   git clone -- "$url" "$dir" && cd "$dir" || return
 }
 
-# shortcuts/defaults
-alias 1p='eval $(op signin)'
-alias c=bat
-alias chn='bat --style=header,numbers'
-alias cn='bat --style=numbers'
-alias cnh='bat --style=header,numbers'
-alias d='docker'
-alias dc='docker-compose'
-alias da='django-admin'
-alias dm='python3 manage.py' # "django manage"
-alias ds='dm shell_plus --ptpython'
-alias dp='cd "$(dirs -pl | tail -n+2 | fzf)"'
-alias dtrx='dtrx --one=inside'
-alias du='du -h'
-alias dud='du -d0 .'
+# go
 dlv() {
   local args=('debug')
   if [[ $# != 0 ]]; then
@@ -156,13 +142,48 @@ dlv() {
   fi
   EDITOR=delve-editor command dlv "${args[@]}"
 }
+go(){ if [[ $# -eq 0 ]]; then rlwrap yaegi; else command go "$@"; fi }
+alias yaegi='rlwrap yaegi'
+
+# homebrew
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+
+# python
+export PIPENV_SHELL_FANCY=1
+export PTPYTHON_CONFIG_HOME=$XDG_CONFIG_HOME/ptpython  # defaults to ~/Library/Application Support/... on Mac
+export PYTHONBREAKPOINT=pudb.set_trace
+export PYTHONDONTWRITEBYTECODE=1
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+alias da='django-admin'
+alias dm='python3 manage.py' # "django manage"
+alias ds='dm shell_plus --ptpython'
+alias py='pyt'
+alias pyb='bpython'
+alias pyc='py -c'
+alias pyi='ptipython'
+alias pym='py -i -c "import pandas as pd; import re; import datetime as dt; from pathlib import Path; import sys; import os; import json; from pprint import pprint as pp;"'
+alias pyt='ptpython'
+
+# shortcuts/defaults/config
+export ERL_AFLAGS="-kernel shell_history enabled" # remember Elixir iex history across sessions
+export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/ripgrep/rc
+alias 1p='eval $(op signin)'
+alias c=bat
+alias chn='bat --style=header,numbers'
+alias cn='bat --style=numbers'
+alias cnh='bat --style=header,numbers'
+alias d='docker'
+alias dc='docker-compose'
+alias dp='cd "$(dirs -pl | tail -n+2 | fzf)"'
+alias dtrx='dtrx --one=inside'
+alias du='du -h'
+alias dud='du -d0 .'
 alias emoji='uni emoji all | fzf | f 0 | cb'
 alias ercho='>&2 echo' # echo to stderr
 alias exists='type &>/dev/null' # check if a program exists
 alias fu='fd -uu' # fd, but don't ignore any files
-alias gh='PAGER= gh' # use gh default pager; gh needs 'less -R' for colors
-alias glab='PAGER= glab' # ...
-go(){ if [[ $# -eq 0 ]]; then rlwrap yaegi; else command go "$@"; fi }
 alias goog='googler -n5 --np'
 alias grep='grep --color=auto'
 alias hex='hexyl'
@@ -180,12 +201,6 @@ alias node="env NODE_NO_READLINE=1 rlwrap node"
 alias notes='te ~/notes/'
 alias pb='[[ $PROMPT_BARE ]] && unset PROMPT_BARE || export PROMPT_BARE=1'
 alias printv='printf "%q\n"' # v for verbatim
-alias py='pyt'
-alias pyt='ptpython'
-alias pyi='ptipython'
-alias pyc='py -c'
-alias pyb='bpython'
-alias pym='py -i -c "import pandas as pd; import re; import datetime as dt; from pathlib import Path; import sys; import os; import json; from pprint import pprint as pp;"'
 alias ssh='sshrc' # always sshrc
 alias tcl='rlwrap tclsh'
 alias title='printf "\e]0;%s\a"' # https://tldp.org/HOWTO/Xterm-Title-3.html#ss3.1
@@ -194,15 +209,6 @@ alias title-win='printf "\e]2;%s\a"'
 user_home() { eval echo "~$1"; } # http://stackoverflow.com/a/20506895
 alias wcl='wc -l'
 alias x='chmod +x'
-alias yaegi='rlwrap yaegi'
-
-# autopager
-alias http='autopager http --pretty=all'
-alias https='autopager https --pretty=all'
-alias jq='autopager jq -C'
-alias curl='autopager curl -L'
-alias xh='autopager xh --pretty=all'
-alias yq='autopager yq -C'
 
 rlh() {
   # reload history
