c9658bda61e18cdf89336ff9d0b718079ecb406d
Keith Devens <keith@keithdevens.com>
2022-08-07 02:26:11 -0600

    Hammerspoon: fix cli install on M1

--- HOME/.hammerspoon/init.lua
+++ HOME/.hammerspoon/init.lua
@@ -5,12 +5,16 @@ hyper = {"cmd", "alt", "ctrl", "shift"}
 -- bind reload at start in case of error later in config
 hs.hotkey.bind(hyper, "R", hs.reload)
 hs.hotkey.bind(hyper, "Y", hs.toggleConsole)
-hs.ipc.cliInstall()
-hs.ipc.cliSaveHistory(true)
 
 function inspect(value)
   hs.alert.show(hs.inspect(value))
 end
+
+-- install cli
+arch = io.popen('uname -p', 'r'):read('*l')
+path = arch == 'arm' and '/opt/homebrew' or nil
+hs.ipc.cliInstall(path)
+hs.ipc.cliSaveHistory(true)
 
 fennel = require("fennel")
 table.insert(package.loaders or package.searchers, fennel.searcher)
