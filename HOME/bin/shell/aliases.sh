4b9a6faac307e14583a9d1bee14cc269e679d36e
Keith Devens <keith@keithdevens.com>
2023-10-28 23:09:06 +0100

    aliases: add 'battery'

--- HOME/bin/shell/aliases.sh
+++ HOME/bin/shell/aliases.sh
@@ -199,6 +199,7 @@ alias pyt='ptpython'
 export ERL_AFLAGS="-kernel shell_history enabled" # remember Elixir iex history across sessions
 export RIPGREP_CONFIG_PATH=$XDG_CONFIG_HOME/ripgrep/rc
 alias 1p='eval $(op signin)'
+alias battery='pmset -g batt'
 alias cbf='fzf | teerr | cb'
 alias d='docker'
 alias dc='docker-compose'
