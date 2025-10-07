cb0bbf3a8a878104add28bffb51c90aa4eb4d62c
Keith Devens <keith@keithdevens.com>
2025-10-07 13:30:04 -0400

    dir colors: pull from vscode's titleBar colors
    - eliminate 'pastel' calls in favor of zsh code thanks to Claude
    - apply Peacock's settings to all available vscode UI elements
    - set "surprise me on startup" = true

--- HOME/bin/shell/prompt.zsh
+++ HOME/bin/shell/prompt.zsh
@@ -17,19 +17,20 @@ precmd() {
   set-kitty-tab-color
 }
 
+rgba-to-rgb() {
+  local c="${1#\#}"
+  [[ ${#c} -ne 8 ]] && { echo "$1"; return; }
+  local r=$((16#${c:0:2})) g=$((16#${c:2:2})) b=$((16#${c:4:2})) a=$((16#${c:6:2}))
+  printf "#%02x%02x%02x" $((r*a/255)) $((g*a/255)) $((b*a/255))
+}
+
 set-kitty-tab-color() {
   [[ $TERM != xterm-kitty || $DIR_COLOR == $OLD_DIR_COLOR ]] && return
-
-  local active_fg inactive_fg
-  if [[ "$DIR_COLOR" ]]; then
-    active_fg=$(pastel textcolor "$DIR_COLOR" | pastel format hex)
-    inactive_fg="#333"; [[ "$active_fg" != "#000000" ]] && inactive_fg="#ccc"
-  fi
-
-  kitty @ set-tab-color --self \
-    active_bg="${DIR_COLOR:-NONE}" inactive_bg="${DIR_COLOR:-NONE}" \
-    active_fg="${active_fg:-NONE}" inactive_fg="${inactive_fg:-NONE}"
-
+  local abg="${DIR_COLOR:-NONE}"
+  local afg="${DIR_COLOR_FG:-NONE}"
+  local ibg="$(rgba-to-rgb "${DIR_COLOR_INACTIVE:-$DIR_COLOR}")"
+  local ifg="$(rgba-to-rgb "${DIR_COLOR_INACTIVE_FG:-$DIR_COLOR_FG}")"
+  kitty @ set-tab-color --self active_bg=$abg inactive_bg=$ibg active_fg=$afg inactive_fg=$ifg
   OLD_DIR_COLOR=$DIR_COLOR
 }
 
