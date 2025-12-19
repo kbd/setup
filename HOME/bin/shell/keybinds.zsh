2e13a3487f371b8bc428c97e228d6fd2a877e01a
Keith Devens <keith@keithdevens.com>
2025-12-19 16:33:05 -0500

    zsh: update keybinds
    - remove obsolete keybinds
    
    Everything still works as expected in: kitty, ghostty, vscode.
    
    - use kitty-launch-and-wait to improve inline editing

--- HOME/bin/shell/keybinds.zsh
+++ HOME/bin/shell/keybinds.zsh
@@ -10,19 +10,12 @@ bindplugin() {
 stty -ixon # allow C-s and C-q to be used for things (see .vimrc)
 bindkey "\e[A" history-beginning-search-backward # ↑
 bindkey "\e[B" history-beginning-search-forward # ↓
-bindkey "\e[1;5D" backward-word # ⌃←
-bindkey "\e[1;5C" forward-word # ⌃→
-bindkey "\e[1;3D" backward-word # ⌥← kitty
-bindkey "\e[1;3C" forward-word # ⌥→
-bindkey "\eb" backward-word # ⌥← vscode
-bindkey "\ef" forward-word # ⌥→
 bindkey "\e[H" beginning-of-line # home
 bindkey "\e[F" end-of-line # end
-bindkey "\e[3~" delete-char # delete
 bindkey "\e[3;3~" kill-word # ⌥del (kitty only, iterm ⌥del==del)
 bindplugin "^[E" edit-command-line # ⌥E
-ecl-ed() { VISUAL="$EDITOR" edit-command-line; }
-bindplugin "^[e" ecl-ed # ⌥e
+.ECL() { VISUAL="kitty-launch-and-wait \"$EDITOR\"" edit-command-line; }
+bindplugin "^[e" .ECL # ⌥e
 
 auto-expand() {
   if [[ $LBUFFER =~ ' [A-Z0-9]+$' ]]; then
